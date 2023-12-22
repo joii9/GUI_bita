e05ce90398f93d789f6853fb1478d92e62b10b28
joii9 <joel.c.mz9@gmail.com>
2023-12-22 17:41:26 -0600

    IT_database now have one event with the new format. funcion_buscar.py is almost ready to launch in bita.py file and miscellaneous.py have partially the function of search

--- extras/funcion_buscar.py
+++ extras/funcion_buscar.py
@@ -1,10 +1,13 @@
 import sqlite3
+import webbrowser
 
-texto="FILTRO"
+texto='"%izopsws%"'
+print(texto)
 
 connection = sqlite3.connect("C:/Users/Joel/Desktop/GUI_bita/IT_database.db")
 cursor = connection.cursor()
-cursor.execute("Select DATEID, TICKET, USERID, EVENT from EVENTS ")
+#print(f"SELECT DATEID, TICKET, USERID, EVENT FROM EVENTS WHERE EVENT LIKE {texto}")
+cursor.execute(f"SELECT DATEID, TICKET, USERID, EVENT FROM EVENTS WHERE EVENT LIKE {texto}")
 rows= cursor.fetchall()
 print(rows)
 
@@ -17,22 +20,13 @@ print(rows[0])
 #print("Y ESTE?")
 #print(chain[0])
 
-def generating_file(rows):
-    #print(rows)
-    #print(type(rows[0]))
-    #chain=str(rows[0])                   #.split(",")
-    #print(chain)
-    
-    head_filter= """
+head="""
     <body bgcolor="black">
     <body text="white">
     <h1>CONSULTA DE EVENTOS</h1>
 
     <h2>Filtro: <em>"""+texto+"""</em> </h2> <!-- texto va a ser el filtro de busqueda en los querys-->
 
-    """
-    
-    table= """
     <table bgcolor="white">
         <tr bgcolor="black">
             <th width=150>FECHAID</th>
@@ -40,18 +34,38 @@ def generating_file(rows):
             <th width=150>USUARIO</th>
             <th width=1300>EVENTO</th>
         </tr>
+        
+
+    """
+
+def generating_file(rows):
+    
+    table= """
         <tr bgcolor="black" align="center">
             <td>"""+str(rows[0])+"""</td> 
             <td>"""+str(rows[1])+"""</td>
             <td>"""+str(rows[2])+"""</td>
             <td align="left">"""+rows[3]+"""</td>
-            </tr>
-        </table>
-    """
-    print(head_filter+table)
-    return head_filter+table
+        </tr>
+            """
+    print(table)
+    return table
+
+end= "</table>"
 
 text=""
 for row in rows:
     text+=generating_file(row)
-print(text)
+#print(text)
+
+print("INICIO")
+print(head+text+end)
+
+f = open("extras/table.html", "w")
+f.write(head+text+end)
+#f.close
+
+f = open("extras/table.html", "r")
+print(f.read())
+
+webbrowser.open("C:/Users/Joel/Desktop/GUI_bita/extras/table.html")
