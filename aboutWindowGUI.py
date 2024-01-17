1b5a1610be157bc88e18b042f387d022c48ab723
joii9 <joel.c.mz9@gmail.com>
2024-01-17 16:49:14 -0600

    aboutWindowGUI was created to show the version who developed this project and my thanks for all the support that I received during this project

---
+++ aboutWindowGUI.py
@@ -0,0 +1,22 @@
+import tkinter
+
+from tkinter import *
+
+from miscellaneous import *
+
+class AboutWindow():
+    
+    def __init__(self):
+        self.about=tkinter.Tk()
+        self.about.title("Acerca de...")
+        self.about.configure(bg="#D49FFF")
+        self.about.geometry("300x250")
+        self.creating_text()
+    
+    def creating_text(self):
+
+        text_ver= Label(self.about, text=text_about, wraplength=500, bg="#D49FFF")
+        text_ver.pack()
+
+
+       
