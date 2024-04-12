a57866b9a6e0cd6ca1878d7b1aa48c4bb9dd4c48
joii9 <joel.c.mz9@gmail.com>
2024-04-12 17:24:59 -0600

    I renamed one method in the object

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bita import MainWindow
 
 main_window = MainWindow()
 main_window.bar_menu()
-main_window.search()
+main_window.create_searchBar()
 main_window.create_title("Eventos pasados")
 main_window.create_table()
 main_window.show()
