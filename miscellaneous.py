c824da2806793ab15c768ddb6945af4ca945fe97
joii9 <joel.c.mz9@gmail.com>
2024-08-07 12:44:46 -0600

    I forgot change the version

--- miscellaneous.py
+++ miscellaneous.py
@@ -9,17 +9,13 @@ str_file=file.read()
 #print(file.read())
 
 path= re.findall("[A-Z].*db", str_file)[0]
-
-
 search= re.findall("[A-Z].*html", str_file)[0]
-
-
 ind= re.findall("[A-Z].*sql", str_file)[0]
 
 
 text_about= """
 BITACORA DE SISTEMAS 
-Version 2.1.0
+Version 2.2.0
 2024
 
 Sistema Satelital Mexicano
