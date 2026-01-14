9fb6933a5b9dde830052e63463c0b2dac624ce2c
joii9 <joel.c.mz9@gmail.com>
2026-01-14 16:15:37 -0600

    I removed the methods of object of this file.

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from bita import MainWindow
 
-main_window = MainWindow()
-main_window.bar_menu()
-main_window.create_searchBar()
-main_window.create_title("Eventos")
-main_window.create_table()
-main_window.show()
+MainWindow() #main_window = MainWindow()
